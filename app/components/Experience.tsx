1a679facec082d82f93791698204623cfff6373c
Ardi Kadriu <ardikadriu156@gmail.com>
2024-01-25 22:52:26 +0100

    more to do...

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Basketweb from "@/public/basketball-web1.png";
 import TaxiWeb from "@/public/taxi-cover.png";
 import SimpleLogin from "@/public/web-phones.png";
-import Signin from "@/public/Sign In Page Design.jpg";
+import Renthouses from "@/public/rent.png";
 import Figma from "@/public/figma.png";
 import Github from "@/public/github.png";
 import { RxVercelLogo } from "react-icons/rx";
@@ -14,7 +14,7 @@ const Experience = () => {
       <h1 className="text-center text-3xl font-semibold font-Raleway mb-5">
         Projects
       </h1>
-      <div className="border border-black px-2 pt-2 mb-4">
+      <div className=" px-2 pt-2 mb-4">
         <Link href={"https://basketball-g48iza31h-ardikadriu.vercel.app"}>
           <Image src={Basketweb} width={240} height={240} alt="WEb" />
         </Link>
@@ -43,24 +43,24 @@ const Experience = () => {
         </div>
       </div>
 
-      <div className="border border-black px-2 pt-2 mb-4">
-        <Link href={"https://nike-layout.vercel.app/"}>
+      <div className=" px-2 pt-2 mb-4">
+        <Link href={"https://taxiapp-mobile.vercel.app/"}>
           <Image src={TaxiWeb} width={240} height={240} alt="WEb" />
         </Link>
         <div className="flex items-center justify-between mt-3">
           <Link
             href={
-              "https://www.figma.com/file/WaWEV7MRljBVpqne552uEI/Sport-Website-(Community)?mode=dev"
+              "https://www.figma.com/file/Jd1A4gexkxCjeXsP72bo1t/Taxi-App-(Community)?node-id=0%3A1&mode=dev"
             }
           >
             <Image src={Figma} width={30} height={40} alt="figma" />
           </Link>
 
-          <Link href={"https://basketball-g48iza31h-ardikadriu.vercel.app"}>
+          <Link href={"https://vercel.com/ardikadriu/taxiapp-mobile"}>
             <RxVercelLogo size={30} />
           </Link>
 
-          <Link href={"https://github.com/ardikadriu/Basketball-web"}>
+          <Link href={"https://github.com/ardikadriu/Taxiapp-mobile"}>
             <Image
               className="object-contain"
               src={Github}
@@ -72,20 +72,20 @@ const Experience = () => {
         </div>
       </div>
 
-      <div className="border border-black px-2 pt-2">
-        <Link href={"https://figma-mobile-design-u1bj.vercel.app//"}>
+      <div className=" px-2 pt-2">
+        <Link href={"https://figma-mobile-design-u1bj.vercel.app/"}>
           <Image src={SimpleLogin} width={240} height={140} alt="WEb" />
         </Link>
         <div className="flex items-center justify-between mt-3">
           <Link
             href={
-              "https://www.figma.com/file/WaWEV7MRljBVpqne552uEI/Sport-Website-(Community)?mode=dev"
+              "https://www.figma.com/file/esbLh7WF0FlnOVOvJ6dNi2/figma-mobile-application-simple-design-(Community)?mode=dev"
             }
           >
             <Image src={Figma} width={30} height={40} alt="figma" />
           </Link>
 
-          <Link href={"https://basketball-g48iza31h-ardikadriu.vercel.app"}>
+          <Link href={"https://vercel.com/ardikadriu/figma-mobile-design-u1bj"}>
             <RxVercelLogo size={30} />
           </Link>
 
@@ -101,21 +101,42 @@ const Experience = () => {
         </div>
       </div>
 
-      <div className="border border-black px-2 pt-2 mt-4">
-        <Link href={"https://sign-in-design.vercel.app/"}>
-          <Image src={Signin} width={240} height={240} alt="WEb" />
+      <div className=" px-2 pt-2 mt-4">
+        <Link href={"https://rent-house-xi.vercel.app/"}>
+          <Image src={Renthouses} width={240} height={240} alt="WEb" />
         </Link>
         <div className="flex">
-          <Link
-            href={
-              "https://www.figma.com/file/PBJ5ZSWb5FfaVD2OhOdOkL/Sign-In-Page-Design-(Community)?node-id=1%3A28&mode=dev"
-            }
-          >
-            <h1 className="text-center w-60 text-2xl font-Raleway font-semibold ">
-              Figmas
-            </h1>
-          </Link>
+          <div className="flex w-full items-center justify-between mt-3">
+            <Link
+              href={
+                "https://www.figma.com/file/1NRkJjfai8AhGC1zOqjkJw/Rent-House-App-(Community)?mode=dev"
+              }
+            >
+              <Image src={Figma} width={30} height={40} alt="figma" />
+            </Link>
+
+            <Link href={"https://vercel.com/ardikadriu/rent-house"}>
+              <RxVercelLogo size={30} />
+            </Link>
+
+            <Link href={"https://github.com/ardikadriu/Rent-house"}>
+              <Image
+                className="object-contain"
+                src={Github}
+                width={30}
+                height={40}
+                alt="github"
+              />
+            </Link>
+          </div>
         </div>
+      </div>
+      <div>
+        <p className="max-w-[250px] text-center font-Raleway mt-6 font-medium">
+          Experienced in crafting 50+ Figma templates. Eager to leverage
+          creative skills and learn quickly in a collaborative development
+          environment.
+        </p>
       </div>
     </div>
   );
